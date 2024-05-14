9549089272cef257f9aeac0b138b95763f0dd726
root <akiraneko@gmail.com>
2024-05-14 18:34:41 +0900

    update

--- update.sh
+++ update.sh
@@ -13,9 +13,9 @@ cd ..
 
 cd datasheet_zh
 # https://www.wch.cn/downloads/CH32V103DS0_PDF.html
-wget --continue https://www.wch-ic.com/downloads/file/311.html -O CH32V103DS0.PDF
+wget --continue https://www.wch.cn/downloads/file/311.html -O CH32V103DS0.PDF
 # https://www.wch.cn/downloads/CH32xRM_PDF.html
-wget --continue https://www.wch-ic.com/downloads/file/328.html -O CH32xRM.PDF
+wget --continue https://www.wch.cn/downloads/file/328.html -O CH32xRM.PDF
 cd ..
 
 # https://www.wch.cn/downloads/CH32V103EVT_ZIP.html
