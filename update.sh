e3a1ad5986561a62f0f543dc1a0b035233e81656
root <tanakamasayuki@gmail.com>
2024-07-05 10:46:03 +0900

    update

--- update.sh
+++ update.sh
@@ -6,20 +6,20 @@ git pull
 
 cd datasheet_en
 # https://www.wch-ic.com/downloads/CH32V103DS0_PDF.html
-wget --continue https://www.wch-ic.com/downloads/file/312.html -O CH32V103DS0.PDF
+curl -z CH32V103DS0.PDF -o CH32V103DS0.PDF https://www.wch-ic.com/downloads/file/312.html
 # https://www.wch-ic.com/downloads/CH32xRM_PDF.html
-wget --continue https://www.wch-ic.com/downloads/file/306.html -O CH32xRM.PDF
+curl -z CH32xRM.PDF -o CH32xRM.PDF https://www.wch-ic.com/downloads/file/306.html
 cd ..
 
 cd datasheet_zh
 # https://www.wch.cn/downloads/CH32V103DS0_PDF.html
-wget --continue https://www.wch.cn/downloads/file/311.html -O CH32V103DS0.PDF
+curl -z CH32V103DS0.PDF -o CH32V103DS0.PDF https://www.wch.cn/downloads/file/311.html
 # https://www.wch.cn/downloads/CH32xRM_PDF.html
-wget --continue https://www.wch.cn/downloads/file/328.html -O CH32xRM.PDF
+curl -z CH32xRM.PDF -o CH32xRM.PDF https://www.wch.cn/downloads/file/328.html
 cd ..
 
 # https://www.wch.cn/downloads/CH32V103EVT_ZIP.html
-wget --continue https://www.wch.cn/downloads/file/326.html -O CH32V103EVT.ZIP
+curl -z CH32V103EVT.ZIP -o CH32V103EVT.ZIP https://www.wch.cn/downloads/file/326.html
 rm -rfv EVT
 unzip -O GB2312 *.ZIP
 
